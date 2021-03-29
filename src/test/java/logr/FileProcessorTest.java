87c713903aebf128ce9c52548ea3832ec4c80fa7
Boris Lukianov <boris.lukianov@seniordev.com>
2021-03-29 17:03:35 +0300

    Line number

--- src/test/java/logr/FileProcessorTest.java
+++ src/test/java/logr/FileProcessorTest.java
@@ -27,17 +27,17 @@ class FileProcessorTest {
         verify(logger, times(11)).info(anyString());
         verify(logger, times(1)).summary(anyString());
 
-        verify(logger).info("\u001B[38;5;3m.1\u001B[0m one \u001B[1;34mtwo\u001B[0m");
-        verify(logger).info("\u001B[38;5;3m.2\u001B[0m \u001B[1;34mtwo\u001B[0m three \u001B[1;34mtwo\u001B[0m");
-        verify(logger).info("\u001B[38;5;3m.3\u001B[0m three four \u001B[1;34mtwo\u001B[0m");
-        verify(logger).info("\u001B[38;5;3m.4\u001B[0m four five \u001B[1;34mtwo\u001B[0m");
-        verify(logger).info("\u001B[38;5;3m.5\u001B[0m five six \u001B[1;34mtwo\u001B[0m");
-        verify(logger).info("\u001B[38;5;3m.6\u001B[0m one \u001B[1;34mtwo\u001B[0m");
-        verify(logger).info("\u001B[38;5;3m.7\u001B[0m \u001B[1;34mtwo\u001B[0m \u001B[1;34mtwo\u001B[0m");
-        verify(logger).info("\u001B[38;5;3m.8\u001B[0m three \u001B[1;34mtwo\u001B[0m");
-        verify(logger).info("\u001B[38;5;3m.9\u001B[0m four \u001B[1;34mtwo\u001B[0m");
-        verify(logger).info("\u001B[38;5;3m10\u001B[0m five \u001B[1;34mtwo\u001B[0m");
-        verify(logger).info("\u001B[38;5;3m12\u001B[0m nine \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m.1\u001B[0m one \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m.2\u001B[0m \u001B[1;34mtwo\u001B[0m three \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m.3\u001B[0m three four \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m.4\u001B[0m four five \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m.5\u001B[0m five six \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m.6\u001B[0m one \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m.7\u001B[0m \u001B[1;34mtwo\u001B[0m \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m.8\u001B[0m three \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m.9\u001B[0m four \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m10\u001B[0m five \u001B[1;34mtwo\u001B[0m");
+        verify(logger).info("\u001B[38;125;7m12\u001B[0m nine \u001B[1;34mtwo\u001B[0m");
         verify(logger).summary("  Total: 11 line(s) in 1 file(s)");
     }
 }
